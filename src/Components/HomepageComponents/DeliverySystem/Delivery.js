d3302747af5df5a436ccdaa35625a4b08c27493b
unknown <emranulfarhad@gmail.com>
2022-05-23 02:19:08 +0600

    delivery trus

--- src/Components/HomepageComponents/DeliverySystem/Delivery.js
+++ src/Components/HomepageComponents/DeliverySystem/Delivery.js
@@ -9,8 +9,26 @@ const Delivery = () => {
 
     return (
         <div>
-            <div className='shadow-2xl rounded-md bg-base-100'>
-                <p>lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, architecto?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae recusandae dolores expedita illum dolore. Culpa voluptatem qui iusto porro aliquam earum quibusdam debitis ipsam provident! Vel optio mollitia veritatis harum repellat odit hic. Expedita illum enim fuga esse assumenda asperiores minima! Delectus eum eaque consectetur, quasi optio eius est dignissimos!</p>
+
+            <div className='shadow-2xl rounded-md bg-base-secondary grid grid-cols-1 lg:grid-cols-4 lg:gap-y-0 gap-y-5 mx-5 mt-20 '>
+              <div className='flex  flex-row mx-10'>
+                  <img src="https://cdn.shopify.com/s/files/1/0266/4166/2049/files/SERVICES_1.png?v=1611834256" alt="" />
+                  <p> </p>
+              </div>
+              <div className='flex  flex-row mx-10'>
+                  <img src="https://cdn.shopify.com/s/files/1/0266/4166/2049/files/SERVICES_2.png?v=1611834279" alt="" />
+                  <p> </p>
+              </div>
+              <div className='flex  flex-row mx-10'>
+                  <img src="https://cdn.shopify.com/s/files/1/0266/4166/2049/files/SERVICES_3.png?v=1611834396" alt="" />
+                  <p> </p>
+              </div>
+              <div className='flex  flex-row mx-10'>
+                  <img src="https://cdn.shopify.com/s/files/1/0266/4166/2049/files/SERVICES_4.png?v=1611834371" alt="" />
+                  <p> </p>
+              </div>
+            
 
             </div>
         </div>
