b4b1bb14d2ce99dfaaeca4885178ce2844f8e62a
unknown <emranulfarhad@gmail.com>
2022-05-24 02:25:52 +0600

    login from done

--- src/Components/Sharecomponents/Login/Login.js
+++ src/Components/Sharecomponents/Login/Login.js
@@ -31,10 +31,9 @@ const Login = () => {
 
                         <div className="absolute bg-gradient-to-b  opacity-75 inset-0 z-0"></div>
                         <div className="w-full  max-w-md z-10">
-                            <div className="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6">Reference site about Lorem Ipsum..</div>
-                            <div className="sm:text-sm xl:text-md text-gray-200 font-normal"> What is Lorem Ipsum Lorem Ipsum is simply dummy
-                                text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever
-                                since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it
+                            <div className="sm:text-4xl xl:text-5xl text-[#FC5A34] font-bold leading-tight mb-6">
+                            APAR NEW USE AUTO PARTS.</div>
+                            <div className="sm:text-sm xl:text-md text-gray-200 font-normal"> We made all of Auto motive body parts Cars motorbike parts any kind of tire helmet mirror , spring soket jumper, engine ,Rim
                                 has?</div>
                         </div>
 
