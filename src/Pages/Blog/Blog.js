ea2ff612044036e3b00bf30ff9cf0015cc7232b7
unknown <emranulfarhad@gmail.com>
2022-05-23 21:01:53 +0600

    card details for products

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -8,18 +8,27 @@ const Blog = () => {
 
     return (
         <div>
-            <div className='mt-20'>
+            <div className='mt-20 mx-20 shadow-2xl flex justify-start flex-col lg:flex-row '>
+                <div className=''>
+                    <img src="https://demo74leotheme.b-cdn.net/prestashop/leo_mtdanstore_demo/70-home_default/customizable-mug.jpg" alt="" />
+                </div>
+                <div className=''>
+                    <h2>-6Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, fugit.</h2>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, fugit.</h2>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, fugit.</h2>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, fugit.</h2>
+                </div>
 
-            <div class="card  w-[50%] card-side bg-base-100 shadow-xl">
-                <figure><img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie"/></figure>
+            </div>
+            <div class="card lg:card-side bg-base-100 shadow-xl container w-[70%]">
+                <figure><img src="https://api.lorem.space/image/album?w=400&h=400" alt="Album" /></figure>
                 <div class="card-body">
-                    <h2 class="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
+                    <h2 class="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
                     <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Watch</button>
+                        <button class="btn btn-primary">Listen</button>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     );
