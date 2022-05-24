bfa959a4cae76a1647320f5baa28a3cdcde6318e
unknown <emranulfarhad@gmail.com>
2022-05-25 01:31:27 +0600

    review add bug fix

--- src/Pages/Review/ReviewsCard.js
+++ src/Pages/Review/ReviewsCard.js
@@ -3,6 +3,8 @@ import './Review.css'
 
 
 const ReviewsCard = () => {
+
+
     return (
         <div>
             <p>ss</p>
@@ -13,8 +15,8 @@ const ReviewsCard = () => {
                     <div class="box">
                         <div class="box-content">
                             <img
-                                src="https://fadzrinmadu.github.io/hosted-assets/design-testimonial-section-using-html-and-css/quote.png" class="quote" alt='a' />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga dolores ipsa impedit ab odit, vel quo, sit maiores sunt enim consectetur architecto, rerum officiis neque animi natus ullam a corporis?</p>
+                                src="https://www.clker.com/cliparts/Y/m/n/S/F/E/orange-closing-quotation-mark-hi.png" class="quote" alt='a' />
+                            <p className='text-left'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga dolores ipsa impedit ab odit, vel quo, sit maiores sunt enim consectetur architecto, rerum officiis neque animi natus ullam a corporis?</p>
                             <img src="https://fadzrinmadu.github.io/hosted-assets/design-testimonial-section-using-html-and-css/1.jpg" class="user" alt='a' />
                             <h3>John Doe</h3>
                         </div>
